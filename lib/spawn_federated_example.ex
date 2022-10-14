3d7dc8eda2367cd9b0232696ef3cb54baf344103
Adriano Santos <sleipnir@bsd.com.br>
2022-10-14 16:31:53 -0300

    Initial project structure

---
+++ lib/spawn_federated_example.ex
@@ -0,0 +1,18 @@
+defmodule SpawnFederatedExample do
+  @moduledoc """
+  Documentation for `SpawnFederatedExample`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SpawnFederatedExample.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/spawn_federated_example_test.exs
@@ -0,0 +1,8 @@
+defmodule SpawnFederatedExampleTest do
+  use ExUnit.Case
+  doctest SpawnFederatedExample
+
+  test "greets the world" do
+    assert SpawnFederatedExample.hello() == :world
+  end
+end
